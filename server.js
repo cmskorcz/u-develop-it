f5d7fc4e5ac44918d4a91a4810ba200bb5654f9a
Chase Skorczewski <chaseskorey@icloud.com>
2022-06-13 14:07:26 -0400

    express connection

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use((req, res) => {
+    res.status(404).end();
+})
+
+app.listen(PORT, () => {
+    console.log(`Application serving on PORT:${PORT}`);
+})
