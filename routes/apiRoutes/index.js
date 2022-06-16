ec066f66ca1b7df9fdb1fc75adb693e44a2aa056
cmskorcz <chaseskorey@icloud.com>
2022-06-16 07:34:04 -0400

    added voter routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const candidateRoutes = require('./candidateRoutes');
-const partyRoutes = require('./partyRoutes')
+const partyRoutes = require('./partyRoutes');
+const voterRoutes = require('./voterRoutes');
 
 router.use(candidateRoutes);
 router.use(partyRoutes);
+router.use(voterRoutes);
 
 module.exports = router;
